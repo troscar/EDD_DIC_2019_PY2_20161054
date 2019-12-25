814817e0b74a7aaa68a069eeab873ec09db67abd
troscar <troscar.ad@gmail.com>
2019-12-25 13:25:07 -0600

    commit 0.1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # EDD_DIC_2019_PY2_20161054
+
+Json Descargar e Instalar :
+1. https://www.youtube.com/watch?v=joAxQtSaErE
+Escribir un Json java:
+2.https://www.youtube.com/watch?v=wDVH3qnXv74 
+leer un Json java:
+3. https://www.youtube.com/watch?v=cFCgFlqF5kw
+
+adjunto videos muestras.
--- src/proyecto2_edd/Proyecto2_EDD.java
+++ src/proyecto2_edd/Proyecto2_EDD.java
@@ -5,9 +5,12 @@
  */
 package proyecto2_edd;
 
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /**
  *
@@ -20,24 +23,24 @@ public class Proyecto2_EDD {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JSONObject obj = new JSONObject();
-        obj.put("Name","MY namen :v");
-        obj.put("Localitazion", "Guate");
-        
-        JSONArray list = new JSONArray();
-        list.add("JAVA");
-        list.add("C++");
-        
-        obj.put("Lenguajes",list);
-        
-        try  {
-            FileWriter file = new FileWriter("myJson.json");
-            file.write(obj.toString());
-            file.flush();
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("myJson.json"));
+            JSONObject jsonObj =(JSONObject)obj;
+            String name = (String)jsonObj.get("Name");
+            System.out.println("Name is: "+name);
+            
+            String location = (String)jsonObj.get("Localitazion");
+            System.out.println("Name is: "+location);
+            
+            JSONArray cousesa = (JSONArray)jsonObj.get("Lenguajes");
+            Iterator<String> iterator = cousesa.iterator();
+            while(iterator.hasNext())
+            {
+                System.out.println("Lenguajes: "+iterator.next());
+            }
         } catch (Exception e) {
         }
-        System.out.println(obj);
-        
         
     }
     
